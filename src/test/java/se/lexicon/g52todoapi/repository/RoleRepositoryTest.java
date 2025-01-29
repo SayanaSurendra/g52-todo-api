cee9fafcac10b0986ebfe64ce00318d83822d487
Sayana Surendran <sayanasurendran31@gmail.com>
2025-01-29 15:54:08 +0100

    part 1 exercises test case

--- src/test/java/se/lexicon/g52todoapi/repository/RoleRepositoryTest.java
+++ src/test/java/se/lexicon/g52todoapi/repository/RoleRepositoryTest.java
@@ -1,9 +1,32 @@
 package se.lexicon.g52todoapi.repository;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import se.lexicon.g52todoapi.domain.entity.Role;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class RoleRepositoryTest {
 
-    //Todo: Write tests for implemented methods
+    Role role;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @BeforeEach
+    void setUp() {
+      role=new Role("admin");
+      roleRepository.save(role);
+    }
+
+    @Test
+    void findByName() {
+      assertNotNull(roleRepository.findByName(role.getName()).get());
+      assertEquals("admin",roleRepository.findByName(role.getName()).get().getName());
+    }
+
 
 }
